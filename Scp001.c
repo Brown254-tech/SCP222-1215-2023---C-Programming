9e76fae39f8117f4c36916ba5e3b392648511f8e
Brown254-tech <kaniarubrown2@gmail.com>
2024-10-15 20:10:31 +0300

    Add files via upload

---
+++ Scp001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d;
+    a = 2;
+    b = 3;
+    c = 4;
+    d = a + b + c;
+    printf("%d + %d + %d = %d\n" , a, b, c, d);
+    return 0;
+}
